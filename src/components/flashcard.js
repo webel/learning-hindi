c47d532149cebf4a19209cd28c5af16760bfe09d
Evita <evitastenqvist@gmail.com>
2020-05-28 21:35:06 +0200

    :poop: :construction: 100mostCommonWords flashcard

--- src/components/flashcard.js
+++ src/components/flashcard.js
@@ -1,28 +1,51 @@
 /** @jsx jsx */
 import { Card, Text, jsx } from "theme-ui"
 
-function speakThis(text) {
+function speak(utterance) {
+  utterance.rate = 0.5
+  utterance.lang = "hi"
+  speechSynthesis.speak(utterance)
+}
+
+function speakThisHindi(text) {
   const utterance = new SpeechSynthesisUtterance(text)
   utterance.rate = 0.5
   utterance.lang = "hi"
   speechSynthesis.speak(utterance)
 }
 
+function speakThisEnglish(text) {
+  const utterance = new SpeechSynthesisUtterance(text)
+  utterance.rate = 0.5
+  utterance.lang = "en"
+  speechSynthesis.speak(utterance)
+}
+
 const ClickableArea = ({ onClick }) => (
-  <div
-    onClick={onClick}
-    sx={{ height: "50%", width: "100% " }}
-  />
+  <div onClick={onClick} sx={{ height: "50%", width: "100% " }} />
 )
+
+export const WordFlashCard = ({ hindi, english, iast = false }) => (
+         <Card
+           css={{ width: [200, 180, 150], height: [200, 180, 150] }}
+           variant="flashCard"
+         >
+           <ClickableArea onClick={() => speakThisHindi(hindi)} />
+           <ClickableArea onClick={() => speakThisEnglish(english)} />
+           <Text css={{ position: "absolute" }} variant="flashCard">
+             {hindi}
+           </Text>
+         </Card>
+       )
 
 const Flashcard = ({ letter, word }) => (
   <Card variant="flashCard">
-    <ClickableArea onClick={() => speakThis(letter)} />
-    <ClickableArea onClick={() => speakThis(word)} />
+    <ClickableArea onClick={() => speakThisHindi(letter)} />
+    <ClickableArea onClick={() => speakThisHindi(word)} />
     <Text css={{ position: "absolute" }} variant="flashCard">
       {letter}
     </Text>
   </Card>
 )
 
-export default Flashcard;
+export default Flashcard
---
+++ src/pages/commonWords.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+import words from "../data/100mostCommonWords.json"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { WordFlashCard } from "../components/flashcard"
+
+/** @jsx jsx */
+import { Card, Text, jsx } from "theme-ui"
+
+// const keys = Object.keys(words)
+
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="100 Most Common Hindi Words" />
+        {words.map(word => (
+            <WordFlashCard key={word.number} {...word} />
+    ))}
+  </Layout>
+)
+
+export default IndexPage
