c8127bced6d6b0bb01ce3bf442f1b1f228af48af
Evita <evitastenqvist@gmail.com>
2020-06-05 21:02:30 +0200

    Better details under letter

--- src/components/detailsUnderFlashcard.js
+++ src/components/detailsUnderFlashcard.js
@@ -1,5 +1,27 @@
 import React from "react"
+/** @jsx jsx */
 import { Card, Text, jsx } from "theme-ui"
+
+function speakThisHindi(text) {
+	const utterance = new SpeechSynthesisUtterance(text)
+	utterance.rate = 0.5
+	utterance.lang = "hi"
+	speechSynthesis.speak(utterance)
+}
+
+const ClickableText = ({ text }) => (
+	<div
+		sx={{
+			px: "5px",
+			backgroundColor: "muted",
+			boxShadow: "0 0 8px rgba(0, 0, 0, 0.125)",
+			display: "inline-block",
+		}}
+		onClick={() => speakThisHindi(text)}
+	>
+		{text}
+	</div>
+)
 
 export const iastEnglishDetails = ({ english, iast }) => {
 	return (
@@ -15,9 +37,15 @@ export const iastEnglishDetails = ({ english, iast }) => {
 export const letterDetails = ({ english, iast, word, soundsLike }) => (
 	<>
 		{iast && <Text my={20}>{iast}</Text>}
-        {(word || english) && <Text mb={20} variant="caps">
-            {word} - {english}
-        </Text>}
-        {soundsLike && <Text mb={20}>{soundsLike}</Text>}
+		{(word || english) && (
+			<div>
+				<ClickableText text={word} />
+				{" - "}
+				<Text mb={20} variant="caps" css={{ display: "inline-block" }}>
+					{english}
+				</Text>
+			</div>
+		)}
+		{soundsLike && <Text mb={20}>{soundsLike}</Text>}
 	</>
 )
