cd7f3ac2fa4bf609eb896ce39b3ef7aac01a659f
Evita <evitastenqvist@gmail.com>
2020-06-05 18:32:47 +0200

    Letter details with word & english & iast & soundslike

--- src/components/detailsUnderFlashcard.js
+++ src/components/detailsUnderFlashcard.js
@@ -10,3 +10,13 @@ export const iastEnglishDetails = ({ english, iast }) => {
             </Text>
         </>)
 }
+
+export const letterDetails = ({ english, iast, word, soundsLike }) => (
+	<>
+		{iast && <Text my={20}>{iast}</Text>}
+        {(word || english) && <Text mb={20} variant="caps">
+            {word} - {english}
+        </Text>}
+        {soundsLike && <Text mb={20}>{soundsLike}</Text>}
+	</>
+)
